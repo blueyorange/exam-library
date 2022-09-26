5bb6f053f02c36ab6e3d7ef5b9ab9f61b3fc154b
Russell <blueyorange@gmail.com>
2022-09-26 15:47:42 +0100

    display images as base64

--- models/question.model.js
+++ models/question.model.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 
+function bufferToBase64(buffer) {
+  return buffer.toString("base64");
+}
+
 const QuestionSchema = new mongoose.Schema({
   number: {
     type: Number,
@@ -8,7 +12,7 @@ const QuestionSchema = new mongoose.Schema({
     type: String,
   },
   tags: [{ type: String }],
-  questionImages: [{ type: Buffer }],
+  questionImages: [{ type: Buffer, get: bufferToBase64 }],
   markSchemeImages: [{ type: Buffer }],
   questionText: { type: String },
   markSchemeText: { type: String },
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,11 +4,8 @@ const auth = require("../auth/auth.js");
 const Question = require("../models/question.model");
 
 router.get("/", auth, async (req, res) => {
-  const questions = await Question.find(
-    {},
-    "-questionImages -markSchemeImages"
-  );
-  console.log(questions);
+  const questions = await Question.find({});
+  console.log(questions[0].questionImages[0].toString("base64"));
   if (req.user) {
     return res.render("index.njk", { currentUser: req.user, questions });
   } else {
