411ada1e36716b1f3020ddd5d3b975fd40b8d5c9
Russell <blueyorange@gmail.com>
2022-09-26 14:55:04 +0100

    full page display

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../auth/auth.js");
+const Question = require("../models/question.model");
 
-router.get("/", auth, (req, res) => {
+router.get("/", auth, async (req, res) => {
+  const questions = await Question.find(
+    {},
+    "-questionImages -markSchemeImages"
+  );
+  console.log(questions);
   if (req.user) {
-    return res.render("index.njk", { currentUser: req.user });
+    return res.render("index.njk", { currentUser: req.user, questions });
   } else {
     return res.redirect("/login");
   }
